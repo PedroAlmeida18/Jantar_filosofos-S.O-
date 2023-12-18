59a2061b3a7092a67d197dfdadb5ce450d3bc3f8
PedroAlmeida18 <pedrohas35@gmail.com>
2023-12-18 20:17:18 -0300

    COMENTÁRIOS PARA A APRERSENTAÇÃO

--- Filosofos/src/Filosofos.java
+++ Filosofos/src/Filosofos.java
@@ -1,12 +1,7 @@
 import java.util.Random;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-
-
-
-public class Filosofos {
-    public class Garfos extends Thread{
+public class Filosofos { 
+    public class Garfos extends Thread{ // thread garfo
         private final int garfoEsquerda;
         private final int garfoDireita ;
         public Garfos(int garfoDireita, int garfoEsquerda){
@@ -14,7 +9,7 @@ public class Filosofos {
             this.garfoEsquerda = garfoEsquerda;
             
         }
-        public void run(){
+        public void run(){ // método para executar a thread
             while(true){
                 try{
                     SorteioDireita();
@@ -29,7 +24,7 @@ public class Filosofos {
             }
         }
         
-        private void SorteioDireita() {
+        private void SorteioDireita() { // Sorteio do Garfo
             int garfoD;
             Random R = new Random();
             garfoD=R.nextInt(5);
@@ -40,32 +35,30 @@ public class Filosofos {
             int garfoE;
             Random e = new Random();
             garfoE=e.nextInt(5);
-            System.out.println("O garfo da direita sorteado "+ garfoEsquerda);
+            System.out.println("O garfo da esquerda sorteado "+ garfoEsquerda);
         }
         
 
     } 
-    public class Filosofo extends Thread{
+    public class Filosofo extends Thread{ //  Classe : Thread para o Filosofo
         String nome ;
         Garfos garfo;
-        int lugra_mesa;
-        public Filosofo(String nome,Garfos garfo,int lugra_mesa ){
+        int lugar_mesa;
+        public Filosofo(String nome,Garfos garfo,int lugar_mesa ){ // construtor da classe
             this.nome = nome;
             this.garfo=garfo;
-            this.lugra_mesa= lugra_mesa;
+            this.lugar_mesa=lugar_mesa;
             System.out.println("o filosofo "+ nome + " sentou a mesa");
-
-
-
         }
-        public void Pensar() throws InterruptedException{
+        public void Pensar() throws InterruptedException{ // método para filosofo pensar
             System.out.println("o filosofo "+ nome+ " está no mundo da lua ");
             Thread.sleep(1000);
 
 
         }
         
-        public void run(){
+        public void run(){ //método para verificar os 
+                           //garfos da direita e esquerda para permitir que a filosofo coma 
 
             while(true){
 
@@ -175,23 +168,28 @@ public class Filosofos {
 
     }
     public void test(){
-            Garfos g1 = new Garfos(5, 1);
+            Garfos g1 = new Garfos(5, 1); // criação dos garfos
             Garfos g2 = new Garfos(1, 2);
             Garfos g3 = new Garfos(2, 3);
             Garfos g4 = new Garfos(3, 4);
             Garfos g5 = new Garfos(4, 5);
+            
+            // Não é necessário inicializar threads para os garfos
+            // Pois os garfos são inicializado como threads no momneto da criação 
 
-            Filosofo filosofo1 = new Filosofo("PEDRO", g1, 1);
-             Filosofo filosofo2 = new Filosofo("JOÃO", g2, 2);
-              Filosofo filosofo3 = new Filosofo("CARLOS", g3, 3);
-               Filosofo filosofo4 = new Filosofo("WALDEMAR", g4, 4);
-                Filosofo filosofo5 = new Filosofo("EZEQUEIL", g5, 5);
+            Filosofo filosofo1 = new Filosofo("PEDRO", g1, 1); // criação dos filosofos
+            Filosofo filosofo2 = new Filosofo("JOÃO", g2, 2);
+            Filosofo filosofo3 = new Filosofo("CARLOS", g3, 3);
+            Filosofo filosofo4 = new Filosofo("WALDEMAR", g4, 4);
+            Filosofo filosofo5 = new Filosofo("EZEQUEIL", g5, 5);
                 
-                new Thread(filosofo1).start();
+                new Thread(filosofo1).start(); // iniciando a thread filoosofo 5 vezes para cada filosofo
                 new Thread(filosofo2).start();
                 new Thread(filosofo3).start();
                 new Thread(filosofo4).start();
                 new Thread(filosofo5).start();
+            // é necessário, pois quero que lógica defindida seja executada em uma thread separada para cada filosofo
+
         }
     
     
